170404a6c5a57385ad2341f273317317d2435460
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-02-08 20:27:31 +0700

    Vibrate when button get tapped

--- app/src/main/java/com/ditaajipratama/score/MainActivity.kt
+++ app/src/main/java/com/ditaajipratama/score/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.ditaajipratama.score
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plusRed.setOnClickListener {
+            vibratePhone()
+        }
+        minusRed.setOnClickListener {
+            vibratePhone()
+        }
+        plusBlue.setOnClickListener {
+            vibratePhone()
+        }
+        minusBlue.setOnClickListener {
+            vibratePhone()
+        }
+
+    }
+
+    fun vibratePhone() {
+        val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(60, VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+        else {
+            vibrator.vibrate(60)
+        }
     }
 }
